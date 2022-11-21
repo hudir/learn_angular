9687bc784970e7d60dd4647cf3f9c43f534fc6b3
hudir <hudirybw@gmail.com>
2022-11-21 11:48:35 +0100

    class binding

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # learn_angular
 follow Angular Tutorial
 
-3 ways of use selector
+### 3 ways of use selector
 - first use as a tag
 ```
 selector: "app-string"
@@ -26,3 +26,37 @@ in html
 ```
 <div app-string></div>
 ```
+
+### what you can not do in the template
+
+- assign a variable => a = 2+2
+- use the js globe variable like window.xxx
+
+- instead you can do it in the class
+
+### Property Binding
+- Attributes and properties are not the same
+- Attributes - HTML
+- Properties - DOM (Document Object Model)
+- Attributes initialize DOM properties and then they are done. Attribute values cannot change once they are initialized.
+- Property values however can change
+
+- set Attributes in angular 
+```
+<input type="text" value="Vishwas" [id]="myId">
+```
+public myId = "testId";
+
+- set none string attributes
+```
+<input type="text" value="Vishwas2" id="{{myId}}" disabled="false">
+// this is not going to work
+ <input type="text" value="Vishwas2" id="{{myId}}" [disabled]="false">
+  <input type="text" value="Vishwas2" id="{{myId}}" bind-disabled="isDisabled">
+ ```
+
+ - set class base on a boolean value
+ ```
+  <h1 [class.text-danger]="hasError">{{"Welcome " + name}}</h1>
+  public hasError = true
+ ```
--- angular_learn/helloWorld/helloWorld/src/app/test/test.component.ts
+++ angular_learn/helloWorld/helloWorld/src/app/test/test.component.ts
@@ -4,16 +4,50 @@ import { Component } from '@angular/core';
   selector: '[app-test]',
   // templateUrl: './test.component.html',
   template: `<div>
-               InLine template
-               12
-             </div>`,
+               welcome {{name}}
+             </div>
+             <h2>{{2+2}}</h2>
+             <h1 class="text-success">{{"Welcome " + name}}</h1>
+             <h1 [class]="successClass">{{"Welcome " + name}}</h1>
+             <h1 class="text-special" [class]="successClass" >{{"Welcome " + name}}</h1>
+             <h1 [class.text-danger]="hasError">{{"Welcome " + name}}</h1>
+
+
+             <h2>{{name.length}}</h2>
+             <h2>{{name.toUpperCase()}}</h2>
+             <h2>{{greetUser()}}</h2>
+             <h2>{{siteUrl}}</h2>
+             <input type="text" value="Vishwas" [id]="myId">
+             <input type="text" value="Vishwas2" id="{{myId}}" [disabled]="isDisabled">
+             <input type="text" value="Vishwas2" id="{{myId}}" bind-disabled="isDisabled">`,
   // styleUrls: ['./test.component.css']
   styles: [`
   div {
     color: red;
   }
+  .text-success {
+    color: green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special {
+    font-style: italic;
+  }
   `]
 })
 export class TestComponent {
+  public name = "a user's name";
+  public siteUrl = window.location.href;
+  public myId = "testId";
+  public isDisabled = true;
+
+  // Class Binding
+  public successClass = "text-success"
+  public hasError = true
+
+  greetUser(): string{
+    return "Hello " + this.name;
+  }
 
 }
