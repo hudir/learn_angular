7bb81c8fcd321b6ef7c12dc3f0d084bcb6ad2439
Zhuo <hudirybw@gmail.com>
2022-12-08 13:55:53 +0100

    setValue and patchValue

--- forms/reactive_forms/src/app/app.component.ts
+++ forms/reactive_forms/src/app/app.component.ts
@@ -9,9 +9,34 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent {
  
   registrationForm = new FormGroup({
-    userName: new FormControl('Vishwas'),
+    userName: new FormControl('Hudir'),
     password: new FormControl(''),
-    confirmPassword: new FormControl('')
+    confirmPassword: new FormControl(''),
+    
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
   });
+
+  loadApiData(){
+    this.registrationForm.patchValue({
+      userName: "Bruce",
+      password: 'test',
+      confirmPassword: "test"
+    })
+
+    // this.registrationForm.setValue({
+    //   userName: "Bruce",
+    //   password: 'test',
+    //   confirmPassword: "test",
+    //   address: {
+    //     city: 'Schoeneweide',
+    //     state: 'Berlin',
+    //     postalCode: '112248'
+    //   }
+    // })
+  }
 
 }
